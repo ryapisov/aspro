0b5da80dd2660cd7610ad329ac62f200639b92b6
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-09-26 00:33:46 +0300

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import {Navigation} from "./navigation/Navigation"
 import {Footer} from "./footer/Footer"
-import {About} from "./sections/about/About"
-import {Banner} from "./sections/banner/Banner"
-import {Blog} from "./sections/blog/Blog"
-import {Connect} from "./sections/connect/Connect"
-import {GetInTouch} from "./sections/get_in_touch/GetInTouch"
-import {OurWork} from "./sections/our_work/OurWork"
-import {Pricing} from "./sections/pricing/Pricing"
-import {Services} from "./sections/services/Services"
-import {Stats} from "./sections/stats/Stats"
-import {StatsVideo} from "./sections/stats_video/StatsVideo"
-import {Team} from "./sections/team/Team"
-import {Testimonial} from "./sections/testimonial/Testimonial"
+import {
+  About, Banner, Blog, Connect, GetInTouch, OurWork,
+  Pricing, Services, Stats, StatsVideo, Team, Testimonial
+} from "./sections/index"
 
 export default () => {
   return (
