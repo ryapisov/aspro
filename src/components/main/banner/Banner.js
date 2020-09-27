490114eaacafd0d7938f5c58f43148db5cc18279
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-09-28 00:17:59 +0300

    Add img banner

--- src/components/main/banner/Banner.js
+++ src/components/main/banner/Banner.js
@@ -1,10 +1,11 @@
 import React from "react"
+import banner from "../../../images/banner.jpg"
 
 export const Banner = () => {
   return (
     <section id="banner">
       <div className="content">
-        БАННЕР
+        <img src={banner} alt="aspro" className="banner"/>
       </div>
     </section>
   )
