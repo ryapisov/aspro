8631fa1f57e06b329bcff39e2abb174b23946c5b
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-10-04 15:45:23 +0300

    Change structure project

---
+++ src/sectionMain/templates.js
@@ -0,0 +1,53 @@
+import logo from '../images/aspro_min.jpg'
+import banner from '../images/banner_min.jpg'
+import iconPhone from '../images/phone.svg'
+import iconEmail from '../images/email.svg'
+
+export const showImageLogo = () => {
+  return `
+    <div class="header">
+      <img src=${logo} class="logo" alt="${'строительная компания aspro'}" />
+    </div>
+  `
+}
+
+export const showLayerBanner = () => {
+  return `
+    <div class=${'layer-banner'}>
+      <img src=${banner} class="logo" alt="${'строительная компания aspro'}" />
+    </div>
+  `
+}
+
+export const showLayerTriangles = () => {
+  return `
+    <div class=${'layer-triangles'}>
+      <div class=${'upper-triangle'}></div>
+      <div class=${'lower-triangle'}></div>
+      <div class=${'right-triangle'}></div>
+      <div class=${'left-triangle'}></div>
+      <div class=${'long-line'}></div>
+      <div class=${'short-line'}></div>
+    </div>
+  `
+}
+
+export const showLayerTextual = () => {
+  return `
+    <div class=${'layer-text'}>
+      <div class=${'contact'}>
+        <h1>ООО "СК&nbsp; АСПРО"</h1>
+        <span class="address">109382, г.Москва, ул. Люблинская, д.141, оф.424 </span>
+        <div class="phone">
+          <img src="${iconPhone}" alt="" />
+          +7(499) 964-46-70 
+        </div>
+        <div class="email">
+          <img src="${iconEmail}" alt="" />
+          info@sk-aspro.ru 
+        </div>
+      </div>
+      <div class=${'menu'}></div>
+    </div>
+  `
+}
