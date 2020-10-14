1d62a8531959b15dbf433fc4fdf4741e0543fb2f
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-10-14 23:02:42 +0300

    Added bootstrap modal

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,3 +1,12 @@
 import '../scss/style.scss'
 import './form-submit'
 import './partners'
+// import 'bootstrap/dist/js/bootstrap'
+// import 'popper.js/dist/popper'
+
+/*====================================
+SLIDER SCRIPTS
+======================================*/
+$('#carousel-slider').carousel({
+  interval: 2000 //TIME IN MILLI SECONDS
+  });
