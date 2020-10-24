1a174aa4f59db2f8120adaef3a1c3162df3003c9
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-10-24 13:58:27 +0300

    Update

--- dist/main.cea8d406.js
+++ dist/main.cea8d406.js
@@ -234,7 +234,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38931" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46477" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
