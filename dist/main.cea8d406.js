533a76663b3e20e8629a3ddef649938bd60bacdb
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-11-02 20:32:40 +0300

    Rename imgs

--- dist/main.cea8d406.js
+++ dist/main.cea8d406.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../images/banner_min.jpg":[["banner_min.b6017a5a.jpg","assets/images/banner_min.jpg"],"assets/images/banner_min.jpg"],"./../images/aspro_min.png":[["aspro_min.bb1e0de8.png","assets/images/aspro_min.png"],"assets/images/aspro_min.png"],"./../images/icons/phone.svg":[["phone.90975775.svg","assets/images/icons/phone.svg"],"assets/images/icons/phone.svg"],"./../images/icons/email.svg":[["email.54457993.svg","assets/images/icons/email.svg"],"assets/images/icons/email.svg"],"./../images/men.png":[["men.c7b4d78f.png","assets/images/men.png"],"assets/images/men.png"],"./../images/women.png":[["women.3875ba87.png","assets/images/women.png"],"assets/images/women.png"],"./../images/logo_znak.png":[["logo_znak.1476fe72.png","assets/images/logo_znak.png"],"assets/images/logo_znak.png"],"./../images/icons/check.svg":[["check.7d1be6bd.svg","assets/images/icons/check.svg"],"assets/images/icons/check.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/javascript/main.js":[function(require,module,exports) {
+},{"./../images/banner.jpg":[["banner.11e2c9bd.jpg","assets/images/banner.jpg"],"assets/images/banner.jpg"],"./../images/aspro.png":[["aspro.8313e593.png","assets/images/aspro.png"],"assets/images/aspro.png"],"./../images/icons/phone.svg":[["phone.90975775.svg","assets/images/icons/phone.svg"],"assets/images/icons/phone.svg"],"./../images/icons/email.svg":[["email.54457993.svg","assets/images/icons/email.svg"],"assets/images/icons/email.svg"],"./../images/men.png":[["men.c7b4d78f.png","assets/images/men.png"],"assets/images/men.png"],"./../images/women.png":[["women.3875ba87.png","assets/images/women.png"],"assets/images/women.png"],"./../images/logo_znak.png":[["logo_znak.1476fe72.png","assets/images/logo_znak.png"],"assets/images/logo_znak.png"],"./../images/icons/check.svg":[["check.7d1be6bd.svg","assets/images/icons/check.svg"],"assets/images/icons/check.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/javascript/main.js":[function(require,module,exports) {
 "use strict";
 
 require("../scss/index.scss");
@@ -234,7 +234,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34925" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43913" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
