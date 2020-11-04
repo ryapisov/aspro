f3d2be876b36625bc9f4192c9b0c60a1c14aa6e3
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-11-04 21:31:20 +0300

    Update

--- version_1.0.2/mail.php
+++ version_1.0.2/mail.php
@@ -13,7 +13,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'dmitriev@sk-aspro.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '*********'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Password = '8a4PRTX1'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
