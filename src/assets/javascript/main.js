f56af27a6f56eefcdc548ab302ec3c4d3e9fa62f
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-11-18 17:57:14 +0300

    Add protection from bots

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -4,6 +4,8 @@ const btn = document.querySelector('#btn-open-form')
 const btnClose = document.querySelector('#btn-close')
 const form = document.querySelector('#form')
 const requisites = document.querySelector('#requisites')
+const textarea = document.querySelector("textarea")
+const checkSuccess = document.querySelector("form")[1]
 
 btn.addEventListener('click', ()=>{
   form.style.display = 'block'
@@ -13,4 +15,10 @@ btn.addEventListener('click', ()=>{
 btnClose.addEventListener('click', ()=>{
   form.style.display = 'none'
   requisites.style.display = 'block'
+})
+
+textarea.addEventListener("keyup", function(e) {
+  if(e.code){
+   checkSuccess.value="form-success-sk-aspro.ru"
+  }
 })
